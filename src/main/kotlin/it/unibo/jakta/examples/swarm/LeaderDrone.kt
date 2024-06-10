aa6b5e6c7591cdc517954d34a771265c25f0830f
Martina Baiardi <m.baiardi@unibo.it>
2024-06-10 11:29:29 +0200

    update jakta framework version

--- src/main/kotlin/it/unibo/jakta/examples/swarm/LeaderDrone.kt
+++ src/main/kotlin/it/unibo/jakta/examples/swarm/LeaderDrone.kt
@@ -7,39 +7,27 @@ import it.unibo.alchemist.jakta.util.fix
 import it.unibo.alchemist.model.Position
 import it.unibo.alchemist.model.molecules.SimpleMolecule
 import it.unibo.jakta.agents.bdi.Agent
-import it.unibo.jakta.agents.bdi.dsl.beliefs.fromPercept
 import it.unibo.jakta.agents.bdi.messages.Achieve
 import it.unibo.jakta.agents.bdi.messages.Message
-import it.unibo.jakta.agents.dsl.mas
-import it.unibo.jakta.examples.swarm.CircleMovement.createCircleCenter
-import it.unibo.jakta.examples.swarm.CircleMovement.degreesToRadians
+import it.unibo.jakta.agents.dsl.device
 import it.unibo.jakta.examples.swarm.CircleMovement.positionInCircumference
 import it.unibo.tuprolog.core.Struct
 import it.unibo.tuprolog.solve.libs.oop.ObjectRef
 import kotlin.math.PI
 
-var leaderCircumferenceDegrees = 180
-
 val destination = SimpleMolecule("desiredPosition")
 
 fun <P : Position<P>> JaktaEnvironmentForAlchemist<P>.leader(): Agent =
-    mas {
+    device {
         environment {
             actions {
                 action("circleMovementStep", 0) {
                     val initialPosition = SwarmPosition.fromPosition(alchemistEnvironment.getPosition(node))
                     val radius = data["radius"] as? Number ?: error("Missing radius as Node molecule. $data")
                     val center = SwarmPosition(0.0, 0.0)//data["centerPosition"]
-//                    if (center == null) {
-//                        center = createCircleCenter(SwarmPosition.fromPosition(initialPosition), radius as Double)
-//                        addData("centerPosition", center)
-//                    }
                     val nextPosition = positionInCircumference(
                         radius.toDouble(),
                         2 * PI * alchemistEnvironment.simulation.time.toDouble() / 600,
-//                        degreesToRadians(leaderCircumferenceDegrees).also {
-//                            leaderCircumferenceDegrees = ((leaderCircumferenceDegrees + 1) % 360) + 1
-//                        },
                         SwarmPosition.fromPosition(center),
                     )
 
@@ -47,15 +35,8 @@ fun <P : Position<P>> JaktaEnvironmentForAlchemist<P>.leader(): Agent =
                     addData("velocity", doubleArrayOf(movement.x, movement.y))
 
                     node.setConcentration(destination, nextPosition)
-//                    alchemistEnvironment.moveNodeToPosition(
-//                        node,
-//                        nextPosition.toPosition(alchemistEnvironment),
-//                    )
                 }
                 action("notifyAgent", 1) {
-                    // "drone@nodeid"
-//                    var participants = (data["participants"] ?: setOf<String>()) as Set<*>
-//                    participants = participants + dest.split("@")[1]
                     val participants = alchemistEnvironment.getNeighborhood(node)
                         .map { it.id }.toSet()
                     val payload: Struct = Struct.of(
@@ -66,19 +47,24 @@ fun <P : Position<P>> JaktaEnvironmentForAlchemist<P>.leader(): Agent =
                     )
                     broadcastMessage(Message(sender, Achieve, payload))
                 }
+                action("storeId", 1) {
+                    val id = argument<ObjectRef>(0).fix<Int>()
+                    addData("id", id)
+                }
             }
         }
         agent("leader") {
-            addData("id", node.id)
             goals {
+                achieve("init")
                 achieve("move")
             }
             plans {
+                +achieve("init") then {
+                    execute("storeId"(node.id))
+                }
                 +achieve("move") then {
-                    // execute("print"("Hi, I'm the leader at node $node.id"))
                     execute("circleMovementStep")
                     execute("notifyAgent")
-                    //execute("sleep"(2000))
                     achieve("move")
                 }
             }
